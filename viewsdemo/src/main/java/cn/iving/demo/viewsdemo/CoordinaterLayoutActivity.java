c011b4834c6da6a45da0e20602f37d41e5079e2a
IvingHome <249301352@qq.com>
2019-05-08 23:53:47 +0800

    RecyclerView demo:使用LayoutManager的基础用法 1.使用LinearLayoutManager 2.使用RecyclerView.Adapter 中的三个方法。

--- viewsdemo/src/main/java/cn/iving/demo/viewsdemo/CoordinaterLayoutActivity.java
+++ viewsdemo/src/main/java/cn/iving/demo/viewsdemo/CoordinaterLayoutActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.iving.demo.viewsdemo.adapter.HangzhouAdapter;
 
 /**
  * 2019,5,6
@@ -16,6 +23,8 @@ public class CoordinaterLayoutActivity extends Activity {
 
     AppBarLayout mAppBarLayout;
     TextView mTvTitle;
+
+    RecyclerView mRecyclerView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +43,20 @@ public class CoordinaterLayoutActivity extends Activity {
             }
         });
 
+        initRecyclerView();
+
+    }
+
+
+
+    private void initRecyclerView(){
+        mRecyclerView = this.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+        List<String> data= new ArrayList<String>();
+        for(int i=0;i<25;i++){
+            data.add("杭州人民欢迎你");
+        }
+        mRecyclerView.setAdapter(new HangzhouAdapter(data));
     }
 }
---
+++ viewsdemo/src/main/java/cn/iving/demo/viewsdemo/adapter/HangzhouAdapter.java
@@ -0,0 +1,58 @@
+package cn.iving.demo.viewsdemo.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.iving.demo.viewsdemo.R;
+
+/**
+ * data: 2019,5,8
+ */
+public class HangzhouAdapter extends RecyclerView.Adapter {
+
+
+    private final List<String> mData;
+
+    public HangzhouAdapter(List<String> data) {
+        mData=data;
+    }
+
+    //创建View缓存；
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_hangzhou,null);
+        return new HangZhouViewHolder(view);
+    }
+
+    //绑定数据；
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+
+        HangZhouViewHolder hangZhouViewHolder = (HangZhouViewHolder)viewHolder;
+        hangZhouViewHolder.mTvText.setText(mData.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData==null?0:mData.size();
+    }
+    //View item;
+    public class HangZhouViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mTvText;
+
+        public HangZhouViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTvText= itemView.findViewById(R.id.tvItem);
+        }
+    }
+
+}
