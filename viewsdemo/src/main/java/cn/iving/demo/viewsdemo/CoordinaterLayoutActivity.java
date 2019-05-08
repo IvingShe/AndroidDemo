bd16134f105c17e136ed6b3078437f01ca25498b
IvingHome <249301352@qq.com>
2019-05-08 22:56:56 +0800

    coordinatorLayout demo: 1.显示可以演示向上滑动可以逐渐显示的Title; 2.显示向上滑动可以驻留的TextView;

--- viewsdemo/src/main/java/cn/iving/demo/viewsdemo/CoordinaterLayoutActivity.java
+++ viewsdemo/src/main/java/cn/iving/demo/viewsdemo/CoordinaterLayoutActivity.java
@@ -3,6 +3,10 @@ package cn.iving.demo.viewsdemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * 2019,5,6
@@ -10,10 +14,25 @@ import android.support.annotation.Nullable;
 
 public class CoordinaterLayoutActivity extends Activity {
 
-
+    AppBarLayout mAppBarLayout;
+    TextView mTvTitle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinaterlayout);
+        mTvTitle=findViewById(R.id.tvTitle);
+        mAppBarLayout=findViewById(R.id.appbarLayout);
+        mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                Log.d("iving","verticalOffset="+verticalOffset+",height="+appBarLayout.getMeasuredHeight());
+                if(-verticalOffset>appBarLayout.getMeasuredHeight()/2){
+                    mTvTitle.setVisibility(View.VISIBLE);
+                }else{
+                    mTvTitle.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
