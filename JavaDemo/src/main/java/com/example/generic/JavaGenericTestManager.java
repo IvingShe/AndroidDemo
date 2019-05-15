6db0268032d10cedbb420eae044811f67bf0b5f7
IvingHome <249301352@qq.com>
2019-05-15 23:40:38 +0800

    lack of code

---
+++ JavaDemo/src/main/java/com/example/generic/JavaGenericTestManager.java
@@ -0,0 +1,80 @@
+package com.example.generic;
+
+import com.example.generic.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Java 泛型测试API
+ */
+public class JavaGenericTestManager {
+
+
+    public  static void test(){
+
+        Test test= new Test();
+        //new Test().testGeneric();
+       // test.testASimpleGenericClass();
+
+
+       // test.testGenericWildcard();
+       // test.testGenericWildcard2();
+        new JavaGenericTestManager().testGenericMethod();
+    }
+
+    /**
+     * 测试泛型及其继承关系
+     */
+    private void testGenericExtend(){
+        //String 是Object的子类 ；则String数组也是Object数组的子类。
+        Object[] objectArray= new String[]{"AA","BB","CC"};
+
+        //String 是Object的子类;  但List<String > 不是List<Object>的子类；
+        List<Object> listObject=null;
+        List<String> listString= Arrays.asList("AB","BC","CD");
+        //listObject = listString;
+
+    }
+
+
+    /**
+     * 测试泛型方法；
+     */
+    private void testGenericMethod(){
+
+        String[] stringArray= new String[]{"AA","BB","CC"};
+        List<String>  stringList= new ArrayList<>();
+        doFromArray2List(stringArray,stringList);//泛型方法使用
+
+        Person[] personArray= new Person[]{new Person("A",12),new Person("B",13),new Person("C",13)};
+        List<Person>  personList= new ArrayList<>();
+
+        doFromArray2List(personArray,personList);//泛型方法使用
+
+
+    }
+
+    /**
+     * 在声明方法的时间，同时声明泛型。在方法返回值、参数列表，以及方法体中都可以使用泛型类型。
+     * @param array
+     * @param list
+     * @param <T>
+     */
+    private <T> void doFromArray2List(T[] array, List<T> list){
+
+        for(int i=0;i<array.length;i++){
+            list.add(array[i]);
+        }
+        for(T t:list){
+            System.out.println(t);
+        }
+
+    }
+
+
+
+
+
+}
---
+++ okhttpdemo/src/main/java/com/example/okhttpdemo/OKhttpDemoActivity.java
@@ -0,0 +1,44 @@
+package com.example.okhttpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OKhttpDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okhttp_demo);
+        initGet();
+    }
+
+
+    private void initGet() {
+        Log.d("OKhttpDemoActivity","initGet");
+        OkHttpClient okhttpClient = new OkHttpClient();//Http中的一些默认配置；
+        Request request = new  Request.Builder().url("http://www.baidu.com").build();//建构者模式创建request
+
+        Call call = okhttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("OKhttpDemoActivity","onFailure:e="+ e);
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("OKhttpDemoActivity","onResponse:="+ response.body().toString());
+            }
+        });
+    }
+}
