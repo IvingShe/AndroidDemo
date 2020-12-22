621f39813724630745840d88601dee4936ec3701
Sheli_ZJ <249301352@qq.com>
2020-12-22 18:39:07 +0800

    启动页面无法显示的问题

--- app/src/main/java/cn/iving/demo/androiddemoapp/MainActivity.java
+++ app/src/main/java/cn/iving/demo/androiddemoapp/MainActivity.java
@@ -47,18 +47,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
         this.findViewById(R.id.btn_camera).setOnClickListener(this);
         this.findViewById(R.id.btn_mvp).setOnClickListener(this);
         //btn_validate_service
