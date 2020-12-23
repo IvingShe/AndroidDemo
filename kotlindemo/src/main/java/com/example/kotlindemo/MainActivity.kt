8ba2b4da659fbb112d64921f01f3bf20efc09a92
Sheli_ZJ <249301352@qq.com>
2020-12-23 17:33:27 +0800

    KotlinModule:测试变量和函数定义

--- kotlindemo/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ kotlindemo/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.kotlindemo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var content :String =""
+    val valContent :String ="You clicked me!"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tv_helloworld.text="Hello,Kotlin"
+        content="Hello,Kotlin"
+        tv_helloworld.text=content
 
         tv_helloworld.setOnClickListener {
-            tv_helloworld.text="you clicked me!"
+
+           // valContent=""
+            tv_helloworld.text=valContent
+            tv_helloworld.setTextColor(Color.GREEN)
+            tv_helloworld.setTextSize(30f);
         }
+
+        var function:String ="1+2="
+
+        tv_testFunction.setOnClickListener {
+            tv_testFunction.text =function+testAdd(1,2)
+        }
+    }
+
+    fun testAdd(a:Int,b:Int):Int{
+       return a+b
     }
 }
